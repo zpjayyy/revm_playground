2471d4d4931833e30da2cd306549276ff4e77e6e
zpjayyy <jay.lichzp@gmail.com>
2023-11-17 16:49:31 +0800

    feat: subscribe transaction

--- src/trace.rs
+++ src/trace.rs
@@ -8,8 +8,16 @@ use cfmms::dex::{Dex, DexVariant};
 use cfmms::sync::sync_pairs;
 use dashmap::DashMap;
 use ethers::prelude::H160;
-use ethers_providers::{Provider, Ws};
+use ethers::types::U256;
+use ethers::types::U64;
+use ethers::types::Transaction;
+use ethers_providers::{Middleware, Provider, Ws};
 use log::info;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::error::SendError;
+use tokio::sync::broadcast::Sender;
+use tokio::task::JoinSet;
+use tokio_stream::StreamExt;
 
 pub async fn mempool_watching(target_address: String) -> Result<()> {
     let wss_url = std::env::var("WSS_URL").unwrap();
@@ -45,5 +53,67 @@ pub async fn mempool_watching(target_address: String) -> Result<()> {
         pools.insert(pool.address(), pool);
     }
     info!("Uniswap v3 pool synced {}", pools.len());
+
+    let (event_sender, _): (Sender<Event>, _) = broadcast::channel(512);
+    let mut set = JoinSet::new();
+
+    {
+        let provider = provider.clone();
+        let event_sender = event_sender.clone();
+        set.spawn(async move {
+            let stream = provider.subscribe_blocks().await.unwrap();
+            let mut stream = stream.filter_map(|block| match block.number {
+                None => None,
+                Some(number) => Some(NewBlock {
+                    number,
+                    gas_used: block.gas_used,
+                    gas_limit: block.gas_limit,
+                    base_fee_per_price: block.base_fee_per_gas.unwrap_or_default(),
+                    timestamp: block.timestamp,
+                })
+            });
+
+            while let Some(block) = stream.next().await {
+                match event_sender.send(Event::NewBlock(block)) {
+                    Ok(_) => {}
+                    Err(_) => {}
+                }
+            }
+        });
+    }
+
+    {
+        let provider = provider.clone();
+        let event_sender = event_sender.clone();
+
+        set.spawn(async move {
+            let stream = provider.subscribe_pending_txs().await.unwrap();
+            let mut stream = stream.transactions_unordered(256).fuse();
+
+            while let Some(result) = stream.next().await {
+                match result {
+                    Ok(tx) => match event_sender.send(Event::Transaction(tx)) {
+                        Result::Ok(_) => {}
+                        Err(_) => {}
+                    }
+                    Err(_) => {}
+                }
+            }
+        });
+    }
     Ok(())
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct NewBlock {
+    pub number: U64,
+    pub gas_used: U256,
+    pub gas_limit: U256,
+    pub base_fee_per_price: U256,
+    pub timestamp: U256,
+}
+
+pub enum Event {
+    NewBlock(NewBlock),
+    Transaction(Transaction),
 }
